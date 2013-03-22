aa0fb6bbf3195876b0e7439b60489f0b33eb4247
vtriquet <virgil.triquet@gmail.com>
2013-03-23 00:48:28 +0100

    #40 Validate Timer.js using a Linter

--- js/classes/Timer.js
+++ js/classes/Timer.js
@@ -1,65 +1,59 @@
 /**************************************************************************************************
-Constructor the Timer class
+Constructor of the Timer class
 **************************************************************************************************/
-function Timer() {
+var Timer = function Timer() {
   this.tSecondsElapsed = 0; // Tenths of seconds elapsed since the timer starts
   this.secondsElapsed = 0; // Seconds elapsed since the timer starts
   this.minutesElapsed = 0; // Minutes elapsed since the timer starts
   this.interval = null; // Interval
-}
+};
 
 /**************************************************************************************************
-Starts the timer
+Prototype the Timer class
 **************************************************************************************************/
-Timer.prototype.start = function() {
-  this.interval = setInterval(function(){
-                oTimer.update();
-               },100);
-}
-
-/**************************************************************************************************
-Pauses the timer
-**************************************************************************************************/
-Timer.prototype.pause = function() {
-  // Stop the execution of the interval
-  clearInterval(this.interval);
-  this.interval = null;
-}
-
-/**************************************************************************************************
-Resets the timer
-**************************************************************************************************/
-Timer.prototype.reset = function() {
-  this.tSecondsElapsed = 0;
-  this.secondsElapsed = 0;
-  this.minutesElapsed = 0;
-}
-
-/**************************************************************************************************
-Updates the timer
-**************************************************************************************************/
-Timer.prototype.update = function() {
-  this.tSecondsElapsed++;
-
-  if (this.tSecondsElapsed > 9) {
+Timer.prototype = {
+  // Starts the timer
+  start: function start() {
+    this.interval = setInterval(function() {
+      oTimer.update();
+    }, 100);
+  },
+  // Pauses the timer
+  pause: function pause() {
+    // Stop the execution of the interval
+    clearInterval(this.interval);
+    this.interval = null;
+  },
+  // Resets the timer
+  reset: function reset() {
     this.tSecondsElapsed = 0;
-    this.secondsElapsed++;
-  }
-
-  if (this.secondsElapsed > 59) {
     this.secondsElapsed = 0;
-    this.minutesElapsed++;
-  }
+    this.minutesElapsed = 0;
+  },
+  // Updates the timer
+  update: function update() {
+    this.tSecondsElapsed = this.tSecondsElapsed + 1;
 
-  if(this.minutesElapsed < 10) {
-    eltTimerMinutes.innerHTML = "0"+this.minutesElapsed;
-  } else {
-    eltTimerMinutes.innerHTML = this.minutesElapsed;
-  }
+    if (this.tSecondsElapsed > 9) {
+      this.tSecondsElapsed = 0;
+      this.secondsElapsed = this.secondsElapsed + 1;
+    }
 
-  if(this.secondsElapsed < 10) {
-    eltTimerSeconds.innerHTML = "0"+this.secondsElapsed;
-  } else {
-    eltTimerSeconds.innerHTML = this.secondsElapsed;
+    if (this.secondsElapsed > 59) {
+      this.secondsElapsed = 0;
+      this.minutesElapsed = this.minutesElapsed + 1;
+    }
+
+    if (this.minutesElapsed < 10) {
+      eltTimerMinutes.innerHTML = "0" + this.minutesElapsed;
+    } else {
+      eltTimerMinutes.innerHTML = this.minutesElapsed;
+    }
+
+    if (this.secondsElapsed < 10) {
+      eltTimerSeconds.innerHTML = "0" + this.secondsElapsed;
+    } else {
+      eltTimerSeconds.innerHTML = this.secondsElapsed;
+    }
   }
-}
+};
