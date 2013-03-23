e1bd146edc8015c8294bdf08d657dfe3e2e5d64f
vtriquet <virgil.triquet@gmail.com>
2013-03-23 11:24:17 +0100

    #40 Validate Menu.js using a Linter

--- js/classes/Menu.js
+++ js/classes/Menu.js
@@ -1,8 +1,7 @@
 /**************************************************************************************************
-Constructor
+Constructor of the Menu class
 **************************************************************************************************/
-function Menu() {
-  // Menu music
+var Menu = function Menu() {
   this.music = new Audio("resources/music/menu.ogg");
   this.music.loop = true;
   this.music.volume = 0.5;
@@ -11,59 +10,53 @@ function Menu() {
 }
 
 /**************************************************************************************************
-Initializes the menu
+Prototype of the Menu class
 **************************************************************************************************/
-Menu.prototype.initialize = function() {
-  // Disable for the moment because it's annoying when we develop
-  fadeOut(eltLaunchScreen, 1);
-  fadeIn(eltMenu, 1);
-  
-  // Set the menu properties
-  eltBtnPlay.style.top = "40%";
-  eltBtnScores.style.top = "45%";
-  eltBtnOptions.style.top = "50%";
-  
-  // Set event listener
-  eltBtnPlay.addEventListener("click", oMenu.clickTabPlay, false);
-  eltBtnScores.addEventListener("click", oMenu.clickTabScores, false);
-  eltBtnOptions.addEventListener("click", oMenu.clickTabOptions, false);
-  eltArrowPrevious.addEventListener("click", oMenu.previous, false);
-  eltBtnLang.addEventListener("click", oMenu.setLanguage, false);
-}
-
-Menu.prototype.previous = function() {
-  eltMenu.style.display = "block";
-  eltOptions.style.display = "none";
-}
-
-Menu.prototype.setLanguage = function() {
-  if (eltBtnLang.src.indexOf("btn-lang-fr") !== -1) {
-    document.webL10n.setLanguage("en");
-    eltBtnLang.src = "resources/images/btn-lang-en.png";
-  } else if (eltBtnLang.src.indexOf("btn-lang-en") !== -1) {
-    document.webL10n.setLanguage("fr");
-    eltBtnLang.src ="resources/images/btn-lang-fr.png";
+Menu.prototype = {
+  // Initilializes the menu
+  initialize: function initialize() {
+    // Disable for the moment because it's annoying when we develop
+    fadeOut(eltLaunchScreen, 1);
+    fadeIn(eltMenu, 1);
+    
+    // Set the menu properties
+    eltBtnPlay.style.top = "40%";
+    eltBtnScores.style.top = "45%";
+    eltBtnOptions.style.top = "50%";
+    
+    // Set event listener
+    eltBtnPlay.addEventListener("click", oMenu.clickTabPlay, false);
+    eltBtnScores.addEventListener("click", oMenu.clickTabScores, false);
+    eltBtnOptions.addEventListener("click", oMenu.clickTabOptions, false);
+    eltArrowPrevious.addEventListener("click", oMenu.previous, false);
+    eltBtnLang.addEventListener("click", oMenu.setLanguage, false);
+  },
+  // Previous function
+  previous: function previous() {
+    eltMenu.style.display = "block";
+    eltOptions.style.display = "none";
+  },
+  // Changes the language of the application. FR and EN only available for the moment
+  setLanguage: function setLanguage() {
+    if (eltBtnLang.src.indexOf("btn-lang-fr") !== -1) {
+      document.webL10n.setLanguage("en");
+      eltBtnLang.src = "resources/images/btn-lang-en.png";
+    } else if (eltBtnLang.src.indexOf("btn-lang-en") !== -1) {
+      document.webL10n.setLanguage("fr");
+      eltBtnLang.src ="resources/images/btn-lang-fr.png";
+    }
+  },
+  // Handles click on the tab "Jouer" / "Play"
+  clickTabPlay: function clickTabPlay() {
+    oGame.initialize();
+  },
+  // Handles click on the tab "Scores"
+  clickTabScores: function clickTabScores() {
+    alert("scores");
+  },
+  // Handles click on the tab "Options"
+  clickTabOptions: function clickTabOptions() {
+    eltMenu.style.display = "none";
+    eltOptions.style.display = "block";
   }
-}
-
-/**************************************************************************************************
-Handles click on the tab "Jouer" / "Play"
-**************************************************************************************************/
-Menu.prototype.clickTabPlay = function() {
-  oGame.initialize();
-}
-
-/**************************************************************************************************
-Handles click on the tab "Scores
-**************************************************************************************************/
-Menu.prototype.clickTabScores = function() {
-  alert("scores");
-}
-
-/**************************************************************************************************
-Handles click on the tab "Options"
-**************************************************************************************************/
-Menu.prototype.clickTabOptions = function() {
-  eltMenu.style.display = "none";
-  eltOptions.style.display = "block";
-}
+};
